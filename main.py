9390c52047d30ff8587f1322e9d1539b105571b9
Pablo7quijivix <davidquijivixsuchi@gmail.com>
2026-09-14 17:49:25 -0600

    Construyendo modulo principal (main) en donde haremos la llamada de todo para la ejecucion del sistema.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(f"Subiendo primer commit al repositorio")
+"""
+El objetivo de este archivo (modulo) es tener un punto de entrada principal 
+para la ejecucion del programa, de esta manera por este modulo se inicializa 
+el gestor de persistencia y llamamos por aca a la interfaz grafica que se ha
+creado con tkinter. De esta manera tenemos organizado todo de manera modular 
+para que todo sea facil de organizar y de entender.
+
+"""
