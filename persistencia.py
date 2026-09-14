20ac97bd3d28a685b099c64df1ec9f55c5e2dda2
Pablo7quijivix <davidquijivixsuchi@gmail.com>
2026-09-14 12:29:56 -0600

    comentario de objetivo del modulo de persistencia

---
+++ persistencia.py
@@ -0,0 +1,9 @@
+'''
+Modulo de persistenica de Datos
+Drescripcion:Este modulo se encarga de la persistencia de datos de la configuracion 
+del usuario almacenado en archivos JSON.
+
+Objetivo de este modulo: Garantiza la escritura y lectura de manera segura, creando respaldos (.bak)
+contiene escritura mediante archivos temporales (.temp), cuenta con manejo robusto de errores como 
+(ausencia, corrupcion, permisos) y codificacion UTF-8
+'''
