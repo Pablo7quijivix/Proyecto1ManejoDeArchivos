78f6e6c793949fda1831d767d53fb4049c0b97e4
Pablo7quijivix <davidquijivixsuchi@gmail.com>
2026-09-14 12:36:58 -0600

    implementacion class GestorPersistencia

--- persistencia.py
+++ persistencia.py
@@ -7,3 +7,19 @@ Objetivo de este modulo: Garantiza la escritura y lectura de manera segura, crea
 contiene escritura mediante archivos temporales (.temp), cuenta con manejo robusto de errores como 
 (ausencia, corrupcion, permisos) y codificacion UTF-8
 '''
+
+import json
+import os 
+import shutil 
+
+
+class GestorPersistencia: 
+    '''
+    Esta clase se encarga de administrar el ciclo de vida del archivo de configuracion,
+    de esta manera estamos asegurando la integridad de los datos cuando se presenten 
+    datos corruptos o posibles fallos de escritura.
+    '''
+    
+    def __init__(self, ruta_archivo= "config.json"):
+        
+    
